05dce6675e6707e086cfdcd29e6046fb81bdee6a
Rubens Tadeu <rubenstadeumoura@gmail.com>
2024-04-04 22:49:31 -0300

    Filtro de busca

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -23,20 +23,32 @@ interface Attendee {
 export function AttendeeList() {
     const [search, setSearch] = useState('')
     const [page, setPage] = useState(1)
+
+    const [total, setTotal] = useState(0)
     const [attendees, setAttendees] = useState<Attendee[]>([])
 
-    const totalPages = Math.ceil(attendees.length / 10)
+    const totalPages = Math.ceil(total / 10)
 
     useEffect(() => {
-        fetch('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+        const url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+
+        url.searchParams.set('pageIndex', String(page - 1))
+
+        if(search.length > 0) {
+            url.searchParams.set('query', search)
+        }
+
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 setAttendees(data.attendees)
+                setTotal(data.total)
             })
-    }, [page])
+    }, [page, search])
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
         setSearch(event.target.value)
+        setPage(1)
     }
 
     function goToFirstPage() {
@@ -61,10 +73,12 @@ export function AttendeeList() {
                 <h1 className="text-2xl font-bold">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg text-sm flex items-center gap-3">
                     <Search className="size-4 text-emerald-300" />
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..." />
+                    <input
+                        onChange={onSearchInputChanged} 
+                        className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"
+                        placeholder="Buscar participante..." 
+                    />
                 </div>
-
-                {search}
             </div>
 
             <Table>
@@ -112,7 +126,7 @@ export function AttendeeList() {
                 <tfoot>
                     <tr>
                         <TableCell colSpan={3}>
-                            Mostrando 10 de {attendees.length} itens
+                            Mostrando {attendees.length} de {total} itens
                         </TableCell>
                         <TableCell className="text-right" colSpan={3}>
                             <div className="inline-flex items-center gap-8">
